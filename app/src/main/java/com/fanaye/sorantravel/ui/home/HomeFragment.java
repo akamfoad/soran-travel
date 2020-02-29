a0874cd4b14d4efd717fb60a5deb4976e7b7567d
akamfoad <41629832+akamfoad@users.noreply.github.com>
2020-02-29 22:39:53 +0300

    changed home fragment and added carousil ImageViewer to it.

--- app/src/main/java/com/fanaye/sorantravel/ui/home/HomeFragment.java
+++ app/src/main/java/com/fanaye/sorantravel/ui/home/HomeFragment.java
@@ -5,32 +5,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.fanaye.sorantravel.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    //    private HomeViewModel homeViewModel;
+    CarouselView carouselview;
+    int[] sampleImages = {R.drawable.flowers, /*R.drawable.forest, */R.drawable.cut, R.drawable.dark};
+
+
+    ImageListener imagelistener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+//        homeViewModel =
+//                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        carouselview = root.findViewById(R.id.carouselmain);
+        carouselview.setPageCount(sampleImages.length);
+        carouselview.setImageListener(imagelistener);
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
